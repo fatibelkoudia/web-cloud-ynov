e45e8e456a2a047a64f82d208b89048756b03267
tima <fatima.ezzahra.belkoudia@gmail.com>
2026-04-30 15:37:14 +0200

    refactor

--- firebase/auth_signup_password.ts
+++ firebase/auth_signup_password.ts
@@ -1,18 +1,10 @@
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 export async function signUpWithEmailAndPassword(email: string, password: string, displayName: string) {
-  try {
-    const auth = getAuth();
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    
-    if (displayName) {
-      await updateProfile(userCredential.user, { displayName });
-    }
-    
-    console.log("Utilisateur créé avec succès:", userCredential.user);
-    return userCredential.user;
-  } catch (error) {
-    console.error("Erreur lors de la création de l'utilisateur:", error);
-    throw error;
+  const auth = getAuth();
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  if (displayName) {
+    await updateProfile(userCredential.user, { displayName });
   }
+  return userCredential.user;
 }
