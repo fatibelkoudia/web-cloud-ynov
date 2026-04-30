b97b4e7eaee7331df6cdfcfec1faf505517c86a8
tima <fatima.ezzahra.belkoudia@gmail.com>
2026-04-30 15:36:36 +0200

    refactor: simplify GitHub authentication provider creation

--- firebase/auth_github_provider_create.ts
+++ firebase/auth_github_provider_create.ts
@@ -1,8 +1,4 @@
 import { GithubAuthProvider } from "firebase/auth";
 
-export function createGithubProvider() {
-  const provider = new GithubAuthProvider();
-  provider.addScope("read:user");
-  provider.addScope("user:email");
-  return provider;
-}
+const provider = new GithubAuthProvider();
+export { provider };
